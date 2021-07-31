a3837d5cd3929415582be7a1d704236a6c9fdb29
masudinn <fachrudin.2000@students.amikom.ac.id>
2021-07-31 23:06:54 +0700

    menampilkan tab screen

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
-import Home from '../page/Home';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Splash from '../page/Splash';
+import Home from '../page/Home';
+import Pesanan from '../page/Pesanan';
+import Akun from '../page/Akun';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainApp = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Pesanan" component={Pesanan} />
+      <Tab.Screen name="Akun" component={Akun} />
+    </Tab.Navigator>
+  );
+};
+
 const Router = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={MainApp} />
       <Stack.Screen name="Splash" component={Splash} />
     </Stack.Navigator>
   );
