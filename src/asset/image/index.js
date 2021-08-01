20a2bf9e46bc285b87a374834e1f88958973916d
masudinn <fachrudin.2000@students.amikom.ac.id>
2021-08-01 22:43:37 +0700

    menampilkan componen header

--- src/asset/image/index.js
+++ src/asset/image/index.js
@@ -1,4 +1,5 @@
 import Logo from '../image/logo.png';
 import Background from '../image/splashbg.png';
+import BackgroundHome from '../image/backgroundhome.png';
 
-export {Logo, Background};
+export {Logo, Background, BackgroundHome};
--- src/page/Home/index.js
+++ src/page/Home/index.js
@@ -1,14 +1,58 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  Dimensions,
+  Image,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import {BackgroundHome, Logo} from '../../asset/image';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <ImageBackground source={BackgroundHome} style={styles.header}>
+        <Image source={Logo} style={styles.logo} />
+        <View style={styles.textheader}>
+          <Text style={{fontSize: 22, fontFamily: 'Roboto-Thin'}}>
+            Selamat Datang,
+          </Text>
+          <Text
+            style={{
+              fontSize: 18,
+              fontFamily: 'Roboto-Bold',
+              marginTop: 7,
+            }}>
+            Udinganga
+          </Text>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  header: {
+    width: windowWidth,
+    height: windowHeight * 0.3,
+  },
+  logo: {
+    width: windowWidth * 0.25,
+    height: windowHeight * 0.06,
+    paddingHorizontal: 30,
+    paddingTop: 10,
+  },
+  textheader: {
+    marginTop: windowWidth * 0.08,
+    marginLeft: windowHeight * 0.03,
+  },
+});
